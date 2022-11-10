77a6431cc39a117f3d2be71829a5ec6ab60227fa
Skyhaneul9 <103580197+Skyhaneul9@users.noreply.github.com>
2022-11-10 14:14:42 -0500

    updated

--- week8/server.js
+++ week8/server.js
@@ -5,20 +5,22 @@ const bodyParser = require("body-parser");
 const handlebars = require("express-handlebars");
 const mongoose = require("mongoose");
 
+//create connectioin, make schema - type, connect with one modulem, add new customer and save, find it
+
 const db1 = mongoose.createConnection("mongodb+srv://admin:<password>@web322.zd2xruj.mongodb.net/db1?retryWrites=true&w=majority");
 
 const customer_schema = new mongoose.Schema({
-    comment: String,
-    author: String,
+    firstname: String,
+    lastname: String,
     date: Date
 });
 const customer = db1.model("customer", customer_schema);
 
 
 var c1 = new customer({ 
-    comment: "sjf",
-    author: "Strifsng",
-    date: "Datdsfe"
+    firstname: "haneul",
+    lastname: "roh",
+    date: "nov"
  });
 
 c1.save((e, data) => {
@@ -30,6 +32,12 @@ c1.save((e, data) => {
   else
   console.log(data);
 });
+
+customer.findOne({ username: loginData.usr, password:loginData.pss }, ["firstname", "lastname", "username"]).exec().then((data) =>{
+            if(data) 
+            {
+             res.render("login", {firstname:data.firstname, lastname:data.lastname, username:data.username, layout: false});
+            }
 
 var port = process.env.PORT || 8080;
 app.listen(port);
